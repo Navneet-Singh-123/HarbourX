a84080145336143ed0c383b3cfb0de3b011483f0
Navneet-Singh-123 <snavneet561@gmail.com>
2021-10-09 17:00:47 +0530

    Some styling changes and modals

--- client/src/components/pages/login.js
+++ client/src/components/pages/login.js
@@ -1,43 +1,58 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import "../../resources/css/index.css";
 
 export default function Login() {
-   
-    const [log,setlog] = useState({email:"",password:""});
+  const [log, setlog] = useState({ email: "", password: "" });
 
-    function handleChange(event){
-        const {name,value}=event.target;
-        setlog(prev=>{
-          return{
-             ...prev,
-             [name] : value,
-          };
-        })
-      }
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setlog((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  }
 
-      function submitlogin(event){
-        setlog({email:"",password:""});
-        event.preventDefault();
-     }
+  function submitlogin(event) {
+    setlog({ email: "", password: "" });
+    event.preventDefault();
+  }
 
-    return (
-      <div className="auth-back">
+  return (
+    <div className="auth-back">
       <div className="auth">
         <h1>Login</h1>
         <form className="login-form">
           <div>
-          <input onChange={handleChange} name="email" value={log.email} placeholder="Email"></input>
+            <input
+              onChange={handleChange}
+              name="email"
+              value={log.email}
+              placeholder="Email"
+              type="email"
+              style={{ padding: "1rem 0", paddingLeft: "1rem" }}
+              required
+            ></input>
           </div>
           <div>
-          <input onChange={handleChange} name="password" value={log.password} placeholder="Password"></input> 
+            <input
+              type="password"
+              onChange={handleChange}
+              name="password"
+              value={log.password}
+              placeholder="Password"
+              style={{ padding: "1rem 0", paddingLeft: "1rem" }}
+              required
+            ></input>
           </div>
-          <div >
-          <button className="form-butt" onClick={submitlogin}>Login</button>
+          <div>
+            <button className="form-butt" onClick={submitlogin}>
+              Login
+            </button>
           </div>
         </form>
       </div>
-      </div>
-    )
-  }
-  
- 
+    </div>
+  );
+}
--- client/src/components/pages/sign-up.js
+++ client/src/components/pages/sign-up.js
@@ -1,48 +1,82 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import "../../resources/css/index.css";
 
 export default function Login() {
-   
-    const [log,setlog] = useState({name:"",email:"",password:"",confirm_password:""});
+  const [log, setlog] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirm_password: "",
+  });
 
-    function handleChange(event){
-        const {name,value}=event.target;
-        setlog(prev=>{
-          return{
-             ...prev,
-             [name] : value,
-          };
-        })
-      }
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setlog((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  }
 
-      function submitlogin(event){
-        setlog({name:"",email:"",password:"",confirm_password:""});
-        event.preventDefault();
-     }
+  function submitlogin(event) {
+    setlog({ name: "", email: "", password: "", confirm_password: "" });
+    event.preventDefault();
+  }
 
-    return (
-
+  return (
     <div className="auth-back">
       <div className="auth">
         <h1>Register</h1>
         <form className="login-form">
           <div>
-          <input onChange={handleChange} name="name" value={log.name} placeholder="Name"></input>
+            <input
+              onChange={handleChange}
+              name="name"
+              value={log.name}
+              placeholder="Name"
+              style={{ padding: "1rem 0", paddingLeft: "1rem" }}
+              required
+            ></input>
           </div>
           <div>
-          <input onChange={handleChange} name="email" value={log.email} placeholder="Email"></input> 
+            <input
+              onChange={handleChange}
+              name="email"
+              value={log.email}
+              placeholder="Email"
+              type="email"
+              style={{ padding: "1rem 0", paddingLeft: "1rem" }}
+              required
+            ></input>
           </div>
           <div>
-          <input onChange={handleChange} name="password" value={log.password} placeholder="Password"></input> 
+            <input
+              onChange={handleChange}
+              name="password"
+              value={log.password}
+              placeholder="Password"
+              type="password"
+              style={{ padding: "1rem 0", paddingLeft: "1rem" }}
+              required
+            ></input>
           </div>
           <div>
-          <input onChange={handleChange} name="confirm_password" value={log.confirm_password} placeholder="Confirm Password"></input> 
+            <input
+              onChange={handleChange}
+              name="confirm_password"
+              value={log.confirm_password}
+              placeholder="Confirm Password"
+              type="password"
+              style={{ padding: "1rem 0", paddingLeft: "1rem" }}
+              required
+            ></input>
           </div>
-          <button className="form-butt" onClick={submitlogin}>Sign Up</button>
+          <button className="form-butt" onClick={submitlogin}>
+            Sign Up
+          </button>
         </form>
       </div>
-      </div>
-    )
-  }
-  
- 
+    </div>
+  );
+}
