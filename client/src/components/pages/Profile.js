20639f8a0fc891ca703db4cfdc81557636830306
Navneet-Singh-123 <snavneet561@gmail.com>
2021-10-09 16:47:00 +0530

    post and journey modals

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../../resources/css/index.css";
-import IMG from "../../resources/images/profile.png";
+import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { Modal, Button } from "react-bootstrap";
 
 const Profile = () => {
+  const [journeymodalShow, setJourneyModal] = useState(false);
+  const [postmodalShow, setPostModal] = useState(false);
+
   const listItems = [
     ["All Journeys", "/all-journeys"],
     ["All Posts", "/all-posts"],
@@ -11,18 +15,79 @@ const Profile = () => {
     ["Create a Post", "/create-post"],
   ];
 
-  const displayList = () =>
-    listItems.map((item) => (
-      <div className="list-container">
-        <Link to={item[1]} className="profile-list-items">
-          {item[0]}
-        </Link>
-      </div>
-    ));
+  const JourneyModal = (props) => {
+    return (
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Create your Journey
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Journey Creation Form</Modal.Body>
+        <Modal.Footer>
+          <Button onClick={props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  };
+
+  const PostModal = (props) => {
+    return (
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Create a Post
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Post Creation Form</Modal.Body>
+        <Modal.Footer>
+          <Button onClick={props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  };
 
   return (
     <div className="profile-container">
-      <ul className="profile-list-content">{displayList()}</ul>
+      <div className="profile-list-content">
+        <div className="list-container">
+          <Link to="/all-journeys" className="profile-list-items">
+            All Journeys
+          </Link>
+          <Link to="/all-posts" className="profile-list-items">
+            All Posts
+          </Link>
+          <div
+            className="profile-list-items"
+            style={{ cursor: "pointer" }}
+            onClick={() => setJourneyModal(true)}
+          >
+            Create a Journey
+          </div>
+          <JourneyModal
+            show={journeymodalShow}
+            onHide={() => setJourneyModal(false)}
+          />
+          <div
+            className="profile-list-items"
+            style={{ cursor: "pointer" }}
+            onClick={() => setPostModal(true)}
+          >
+            Create a Post
+          </div>
+          <PostModal show={postmodalShow} onHide={() => setPostModal(false)} />
+        </div>
+      </div>
       <div className="profile-content">
         <div className="profile-content-container">
           <div className="profile-pic">
